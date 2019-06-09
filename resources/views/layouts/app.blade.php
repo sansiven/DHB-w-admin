a0372edabb994948360bd0b6385f96cdb3a0d7f3
Sabin <sansiven@gmail.com>
2019-06-09 11:28:24 +0545

    App layout for DHB frontend.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,78 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, maximum-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<title>Dhulikhel Boutique Hotel | Best Boutique Hotel in Dhulikhel, Nepal</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+<meta name="robots" content="index,follow">
+<meta name="Googlebot" content="index, follow"/>
+<meta name="distribution" content="Global">
+<meta name="revisit-after" content="2 Days" />
+<meta name="classification" content="Hotel, Hotels in Nepal" />
+<meta name="category" content="Hotel, Hotels in Nepal" />
+<meta name="language" content="en-us" />
+<link rel="canonical" href="http://www.dhulikhelboutiquehotel.com/" />
+<base url="http://www.dhulikhelboutiquehotel.com/"/>
+<meta name="description" content="Dhulikhel Boutique Hotel situated just a km outside of dhulikhel buspark,provides a unique and beautiful experience of your stay. Contact us to get this experience." />
+<meta name="keywords" content="Dhulikhel Boutique Hotel, Himalayas,best hotel in dhulikhel, luxury,good food,Reservation, Travel Nepal,Travel Dhulikhel, Accommodation Nepal, Accommodation Dhulikhel, affordable hotel, best place to have night out near kathmandu, hotels near kathmandu" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129771120-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  gtag('config', 'UA-129771120-1');
+</script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<link rel="icon" href="/img/new%20logo.png" type="image/png">
+<link rel="shortcut icon" href="/img/new%20logo.png" type="img/x-icon">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800italic,700italic,600italic,400italic,300italic,800,700,600' rel='stylesheet' type='text/css'>
+
+<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link href="/css/font-awesome.css" rel="stylesheet" type="text/css">
+<link href="/css/responsive.css" rel="stylesheet" type="text/css">
+<link href="/css/animate.css" rel="stylesheet" type="text/css">
+
+<!--[if IE]><style type="text/css">.pie {behavior:url(PIE.htc);}</style><![endif]-->
+
+<script type="text/javascript" src="/js/jquery.1.8.3.min.js"></script>
+<script type="text/javascript" src="/js/bootstrap.js"></script>
+<script type="text/javascript" src="/js/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="/js/jquery.isotope.js"></script>
+<script type="text/javascript" src="/js/wow.js"></script>
+<script type="text/javascript" src="/js/classie.js"></script>
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+    <nav class="main-nav-outer" id="test"><!--main-nav-start-->
+        <div class="container">
+        <div class="header--logo">
+                <a href="#header"><img src="/img/new%20logo.png" alt="dhulikhel"></a>
             </div>
-        </nav>
+            <ul class="main-nav fl-rt">
+                <li><a href="#header">Home</a></li>
+            <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#Portfolio">Gallery</a></li>
+                <li><a href="#review">Review</a></li>
+                
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <a class="res-nav_click" href="#"><i class="fa-bars"></i></a>
+        </div>
+    </nav><!--main-nav-end-->
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <div class="container">
+        @yield('content')
     </div>
+
 </body>
 </html>
